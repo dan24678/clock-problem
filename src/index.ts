b83fa1ecb4eb94ef9f09262e146e34ffe5f8fb5a
Dan D <dan24678@gmail.com>
2026-05-28 08:51:00 -0400

    Finished initial implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,79 @@
-import LEDNumber from types/types;
+import { LEDNumbers, ClockDigit } from './types/types';
+import process from 'process';
 
-const one = new LEDNumber(1);
-console.log(one.getValue());
+const positions = new LEDNumbers();
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const h1 = new ClockDigit('h1', null, []);
+const h2 = new ClockDigit('h2', null, []);
+const m1 = new ClockDigit('m1', null, []);
+const m2 = new ClockDigit('m2', null, []);
+
+const updateClockDigits = (hours: number, mins: number) => {
+    if (hours < 10) {
+        h1.value = null;
+        h1.positions = [];
+        h2.value = hours;
+        h2.positions = positions.get(hours);
+    } else {
+        h1.value = 1;
+        h1.positions = positions.get(1);
+        h2.value = hours % 10;
+        h2.positions = positions.get(hours % 10);
+    }
+    if (mins < 10) {
+        m1.value = 0;
+        m1.positions = positions.get(0);
+        m2.value = mins;
+        m2.positions = positions.get(mins);
+    } else {
+        m1.value = Math.floor(mins / 10);
+        m1.positions = positions.get(Math.floor(mins / 10));
+        m2.value = mins % 10;
+        m2.positions = positions.get(mins % 10);
+    }
+};
+
+const totals: { [key: string]: number } = {};
+const updateTotals = () => {
+    [h1, h2, m1, m2].forEach(digit => {
+        if (digit.positions.length > 0) {
+            digit.positions.forEach(pos => {
+                const key = `${digit.name}-${pos}`;
+                totals[key] = (totals[key] || 0) + 1;
+            });
+        }
+    });
+}
+
+async function showDisplay(hours: number, mins: number) {
+    process.stdout.write(`${hours}:${mins}`);
+    //process.stdout.write(`${hours}:${mins} -- ${m2.positions.join(', ')}\n`);
+
+    await sleep(10);
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+}
+
+// Main program loop
+async function main() {
+    for (let i=0; i<720; i++) {
+        let hours, mins;
+        if (i < 60) {
+            hours = 12;
+            mins = Math.floor(i % 60);
+        } else {
+            hours = Math.floor(i / 60);
+            mins = Math.floor(i % 60);
+        }
+
+        updateClockDigits(hours, mins);
+        updateTotals();
+        await showDisplay(hours, mins);
+    }
+    const sortedTotals = Object.entries(totals).sort(([,a], [,b]) => b - a);
+    console.log('Totals:', sortedTotals);
+}
+
+main();
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,90 @@
-class LEDNumber {
-    private value: number;
+type Position = 'top' | 'topLeft' | 
+    'topRight' | 'middle' | 'bottomLeft' | 
+    'bottomRight' | 'bottom';
 
-    constructor(value: number) {
-        this.value = value;
+class ClockDigit {
+    private _name: string;
+    private _value: number | null;
+    private _positions: Position[];
+
+    constructor(name: string, value: number | null, positions: Position[] = []) {
+        this._name = name;
+        this._value = value;
+        this._positions = positions;
     }
 
-    public getValue(): number {
-        return this.value;
+    public get name(): string {
+        return this._name;
     }
 
-    public setValue(value: number): void {
-        this.value = value;
+    public set name(newValue: string) {
+        this._name = newValue;
+    }
+
+    public get value(): number | null {
+        return this._value;
+    }
+
+    public set value(newValue: number | null) {
+        this._value = newValue;
+    }
+
+    public get positions(): Position[] {
+        return this._positions;
+    }
+
+    public set positions(newPositions: Position[]) {
+        this._positions = newPositions;
     }
 }
 
-export default LEDNumber;
+class LEDNumber {
+    private _positions: Position[];
+    private _value: number;
+
+    constructor(value: number, positions: Position[] = []) {
+        this._value = value;
+        this._positions = positions;
+    }
+
+    public get value(): number {
+        return this._value;
+    }
+
+    public get positions(): Position[] {
+        return this._positions;
+    }
+}
+
+class LEDNumbers {
+    private numbers: LEDNumber[];
+
+    constructor() {
+        this.numbers = [
+            new LEDNumber(0, ['top', 'topLeft', 'topRight', 'bottomLeft', 'bottomRight', 'bottom']),
+            new LEDNumber(1, ['topRight', 'bottomRight']),
+            new LEDNumber(2, ['top', 'topRight', 'middle', 'bottomLeft', 'bottom']),
+            new LEDNumber(3, ['top', 'topRight', 'middle', 'bottomRight', 'bottom']),
+            new LEDNumber(4, ['topLeft', 'topRight', 'middle', 'bottomRight']),
+            new LEDNumber(5, ['top', 'topLeft', 'middle', 'bottomRight', 'bottom']),
+            new LEDNumber(6, ['top', 'topLeft', 'middle', 'bottomLeft', 'bottomRight', 'bottom']),
+            new LEDNumber(7, ['top', 'topRight', 'bottomRight']),
+            new LEDNumber(8, ['top', 'topLeft', 'topRight', 'middle', 'bottomLeft', 'bottomRight', 'bottom']),
+            new LEDNumber(9, ['top', 'topLeft', 'topRight', 'middle', 'bottomRight', 'bottom']),
+        ];
+    }
+
+    public addNumber(number: LEDNumber): void {
+        this.numbers.push(number);
+    }
+
+    public get(numValue: number): Position[] {
+        return this.numbers[numValue]?.positions || [];
+    }
+}
+
+export { 
+    ClockDigit,
+    LEDNumber, 
+    LEDNumbers 
+};
